96b41f7c02a643b1a19ca7dc23380c81ffcaf00c
apinesh55 <apineshtharmarasa105@gmail.com>
2025-04-29 21:03:50 +0530

    editted 11_string

--- 11_string_function.c
+++ 11_string_function.c
@@ -4,8 +4,17 @@ int main()
 {
     char a[]={'A','B','I','\0'};
     printf("%s",a);
-    char b[20];
+    char b[20],c[20];
     printf("\n Enter the String: ");
-    scanf("%s",b); //& is not needed because b is arry
+    gets(b); // gets = get string
+    printf("\nLength: %d",strlen(b));
+    printf("\nUpper case: %s",strupr(b));
+    printf("\nLower case: %s",strlwr(b));
+    printf("\nCopy a vlue to c: %s",strcpy(c,b));
+    printf("\nCompare b,a: %d",strcmp(b,a)); //if it is 0, no differents
+    strcat(a,b);
+    printf("\nConcatenation: %s",a);
+    printf("\nReverse: %s",strrev(b));
+
     return 0;
 }
