18933eba37175de8d7990c468aa5062e5203d9a9
apinesh55 <apineshtharmarasa105@gmail.com>
2025-05-02 04:26:27 -0300

    add file math_function

---
+++ 12_Math_function.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    printf("Squre root: %0.2f\n",sqrt(25));
+    printf("Power: %0.2f\n",(2,10));
+    printf("Absolute value of -100: %d\n",abs(-100));
+    printf("Ceiling: %0.2f\n",ceil(4.8));
+    printf("Ceiling: %0.2f\n",ceil(4.2));
+    printf("Floor: %0.2f\n",floor(4.8));
+    printf("Floor: %0.2f\n",floor(4.2));
+    printf("Round: %0.2f\n",round(4.8));
+    printf("Round: %0.2f\n",round(4.2));
+    
+    return 0;
+}
